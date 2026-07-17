f9a90ee9d7157017300d664e86cdc710195041f7
MuhammadBintang27 <muhammad.bintang.indra@gmail.com>
2026-07-17 20:55:38 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,19 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'webhooks/midtrans',
         ]);
+
+        // Railway (dan PaaS sejenis) men-terminate TLS di reverse proxy lalu meneruskan
+        // request ke container lewat HTTP biasa. Tanpa ini, Laravel menganggap semua
+        // request masuk sebagai http:// (mengabaikan header X-Forwarded-Proto), yang
+        // membuat signed URL (mis. guest.pemesanan.show) selalu gagal validasi karena
+        // skema saat generate ("https") beda dengan skema saat validasi ("http").
+        // IP proxy Railway dinamis/tidak diketahui di muka, jadi "*" dipercaya di sini
+        // seperti direkomendasikan Laravel untuk load balancer/PaaS.
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR
+            | Request::HEADER_X_FORWARDED_HOST
+            | Request::HEADER_X_FORWARDED_PORT
+            | Request::HEADER_X_FORWARDED_PROTO
+            | Request::HEADER_X_FORWARDED_AWS_ELB);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
